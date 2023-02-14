2a8be8f33f02b3a29a66d8a80b4083cd9a77295e
PingjunChen <pingjunchen@ieee.org>
2023-02-14 01:30:00 -0600

    add convert_slide

---
+++ Dockerfile
@@ -0,0 +1,50 @@
+FROM nvidia/cuda:11.6.0-devel-ubuntu20.04
+
+# Setting environment variables
+ENV DEBIAN_FRONTEND noninteractive
+ENV HDF5_USE_FILE_LOCKING FALSE
+ENV NUMBA_CACHE_DIR /tmp
+
+RUN rm /etc/apt/sources.list.d/cuda.list && \
+    apt-key del 7fa2af80 && \
+    apt-get update && apt-get install -y --no-install-recommends wget && \
+    wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb && \
+    dpkg -i cuda-keyring_1.0-1_all.deb 
+
+RUN apt-get update && apt-get install -y --no-install-recommends \
+  build-essential libgl1-mesa-glx libglib2.0-0 libgeos-dev libvips-tools \
+  curl sudo htop git wget vim ca-certificates python3-openslide \
+  && rm -rf /var/lib/apt/lists/*
+
+# Install Miniconda
+WORKDIR /App
+RUN chmod 777 /App
+RUN curl -LO http://repo.continuum.io/miniconda/Miniconda3-py38_4.8.2-Linux-x86_64.sh \
+ && bash Miniconda3-py38_4.8.2-Linux-x86_64.sh -p /App/miniconda -b \
+ && rm Miniconda3-py38_4.8.2-Linux-x86_64.sh
+ENV PATH=/App/miniconda/bin:$PATH
+## Create a Python 3.8.3 environment
+RUN /App/miniconda/bin/conda install conda-build \
+ && /App/miniconda/bin/conda create -y --name py383 python=3.8.3 \
+ && /App/miniconda/bin/conda clean -ya
+ENV CONDA_DEFAULT_ENV=py383
+ENV CONDA_PREFIX=/App/miniconda/envs/$CONDA_DEFAULT_ENV
+ENV PATH=$CONDA_PREFIX/bin:$PATH
+
+# Install python packages
+RUN pip install gpustat==0.6.0 setuptools==61.2.0 pytz==2021.1 termcolor==1.1.0
+RUN conda install pytorch torchvision torchaudio pytorch-cuda=11.6 -c pytorch -c nvidia
+RUN pip install openslide-python==1.2.0 opencv-python==4.5.4.60 umap-learn==0.5.3 xgboost==1.5.1 
+RUN pip install histocartography==0.2.1 scikit-image==0.18.0 mahotas==1.4.12 
+RUN pip install tqdm==4.64.0 docopt==0.6.2 imgaug==0.4.0 joblib==1.2.0 termcolor2==0.0.3
+
+# Set environment variables
+WORKDIR /.dgl
+RUN chmod 777 /.dgl
+WORKDIR /.local
+RUN chmod 777 /.local
+WORKDIR /Data
+RUN chmod 777 /Data
+WORKDIR /App/Pyramidal-Tiff
+
+CMD ["/bin/bash"]
---
+++ convert_slide.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+import os, sys
+import shutil, argparse, pytz
+from datetime import datetime
+import openslide
+import numpy as np
+from skimage import io
+import PIL
+from PIL import Image
+PIL.Image.MAX_IMAGE_PIXELS = 933120000
+
+from utils import numpy2tiff
+
+
+def set_args():
+    parser = argparse.ArgumentParser(description = "Convert WSI to Pyramidal TIFF")
+    parser.add_argument("--data_root",         type=str,       default="/Data")
+    parser.add_argument("--raw_slide_dir",     type=str,       default="RawSlides")
+    parser.add_argument("--raw_slide_suffix",  type=str,       default=".svs")
+    parser.add_argument("--tiff_slide_dir",    type=str,       default="TiffSlides")
+
+    args = parser.parse_args()
+    return args
+
+
+if __name__ == "__main__":
+    args = set_args()
+
+    # Raw slide directory
+    raw_slide_dir = os.path.join(args.data_root, args.raw_slide_dir)
+    slide_lst = sorted([os.path.splitext(ele)[0] for ele in os.listdir(raw_slide_dir) if ele.endswith(args.raw_slide_suffix)])
+    # Tiff slide directory
+    tiff_slide_dir = os.path.join(args.data_root, args.tiff_slide_dir)
+    if os.path.exists(tiff_slide_dir):
+        shutil.rmtree(tiff_slide_dir)    
+    os.makedirs(tiff_slide_dir)
+
+    # norm slides one-by-one
+    for idx, cur_slide_name in enumerate(slide_lst):
+        cur_time_str = datetime.now(pytz.timezone('America/Chicago')).strftime("%m/%d/%Y, %H:%M:%S")
+        print("@ {} Norm {}/{} {}".format(cur_time_str, idx+1, len(slide_lst), cur_slide_name))
+        # load slide
+        raw_slide_path = os.path.join(raw_slide_dir, cur_slide_name + args.raw_slide_suffix)
+        slide_head = openslide.OpenSlide(raw_slide_path)
+        slide_w, slide_h = slide_head.dimensions
+        wsi_img = slide_head.read_region(location=(0, 0), level=0, size=(slide_w, slide_h))
+        wsi_img = np.asarray(wsi_img)[:, :, :3]
+        # save to tif
+        tif_img_path = os.path.join(tiff_slide_dir, cur_slide_name + ".tif")
+        numpy2tiff(wsi_img, tif_img_path)
+        # convert the tif to pyramid tiff
+        tiff_slide_path = os.path.join(tiff_slide_dir, cur_slide_name + ".tiff")
+        conversion_cmd_str = " ".join(["vips", "im_vips2tiff", tif_img_path, tiff_slide_path + ":jpeg:75,tile:256x256,pyramid"])
+        os.system(conversion_cmd_str) 
+        if os.path.exists(tif_img_path):
+            os.remove(tif_img_path)
